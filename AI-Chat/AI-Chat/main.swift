83578c777e328065073d282ed138a92206b62a38
asisiov <asisiov@gmail.com>
2026-04-13 15:40:07 +0300

    - initial project

---
+++ AI-Chat/AI-Chat/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AI-Chat
+//
+//  Created by Oleksandr on 13.04.2026.
+//
+
+import Foundation
+import LamaChatModule
+
+print("Hello, World!")
+
---
+++ LamaChatModule/Sources/LamaChatModule/Services/LLMDownloader.swift
@@ -0,0 +1,14 @@
+//
+//  LLMDownloader.swift
+//  LamaChatModule
+//
+//  Created by Oleksandr on 13.04.2026.
+//
+
+public protocol LLMDownloader: Actor {
+    
+}
+
+public actor LLMDownloaderImpl: LLMDownloader {
+    
+}
